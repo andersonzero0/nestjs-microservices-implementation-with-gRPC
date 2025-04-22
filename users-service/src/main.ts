245191f9474f34c57e8c4b3ea361ea96df0e13b3
Anderson Viana <anderson11viana@gmail.com>
2025-04-22 16:37:12 -0300

    Refactor main.ts to add support for dynamic port configuration

--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 // C
 
+// 1
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
